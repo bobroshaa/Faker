4292042593a5cc644a9c1e88fc35658b2ee300bb
bobroshaa <bobroshaa@mail.ru>
2022-10-15 19:28:19 +0300

    add: add generator for classes

--- Faker.Core/Faker.cs
+++ Faker.Core/Faker.cs
@@ -8,26 +8,77 @@ public class Faker
 {
     private List<IValueGenerator> _generators;
     
+    // Get all generators
     public Faker()
     {
         _generators = Assembly.Load("Generators").GetTypes()
             .Where(t => t.GetInterfaces().Contains(typeof(IValueGenerator)))
             .Select(t => (IValueGenerator)Activator.CreateInstance(t)).ToList();
     }
+    
     public T Create<T>()
     {
         return (T) Create(typeof(T));
     }
+    
     public object Create(Type t)
     {
+        object instance = null;
+        // Check if this variable of primitive type
         foreach (var generator in _generators)
         {
             if (generator.CanGenerate(t))
             {
-                return generator.Generate(t);
+                instance = generator.Generate(t);
             }
         }
-        return GetDefaultValue(t);
+        // Try to get constructors of class
+        if (instance == null)
+        {
+            object[] args = GetBestConstructor(t).GetParameters().Select(x => Create(x.ParameterType)).ToArray();
+            instance = Activator.CreateInstance(t, args);
+            GetFields(t, instance);
+            GetProperties(t, instance);
+        }
+        if (instance == null)
+            instance = GetDefaultValue(t);
+        return instance;
+    }
+
+    public ConstructorInfo GetBestConstructor(Type type)
+    {
+        ConstructorInfo[] constructors = type.GetConstructors();
+        var res = constructors[0];
+        foreach (var constructor in constructors)
+        {
+            if (constructor.GetParameters().Length > res.GetParameters().Length)
+                res = constructor;
+        }
+        return res;
+    }
+
+    public void GetFields(Type type, object instance)
+    {
+        FieldInfo[] fields = type.GetFields();
+        foreach (var field in fields)
+        {
+            if (field.GetValue(instance) == null)
+            {
+                field.SetValue(instance, Create(field.FieldType));
+            }
+        }
+    }
+    
+    public void GetProperties(Type type, object instance)
+    {
+        PropertyInfo[] properties = type.GetProperties();
+        foreach (var property in properties)
+        {
+            if (property.GetValue(instance) == null || property.GetValue(instance).Equals(0))
+            {
+                property.SetValue(instance, Create(property.PropertyType));
+            }
+        }
     }
     private static object GetDefaultValue(Type t)
     {
--- Faker.Core/Program.cs
+++ Faker.Core/Program.cs
@@ -12,5 +12,18 @@ class Program
         Console.WriteLine("string" + faker.Create<string>());
         Console.WriteLine("double" + faker.Create<double>());
         Console.WriteLine("long" + faker.Create<long>());
+        User user = faker.Create<User>();
+        Console.WriteLine("user" + " " + user.Age + " " + user.Name);
     }
+}
+
+class User
+{
+    /*public User(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }*/
+    public string Name { get; set; }
+    public int Age { get; set; }
 }
